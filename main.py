ca8cfe02935dc98859eca049c0bdd4789f376d8f
Alfred Reji <alfredreji@MacBookAir.communityfibre.co.uk>
2026-04-17 14:12:50 +0100

    Initial application and Dockerfile

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import socket
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {
+        "message": "Hello from Alfred's ECS deployment",
+        "container": socket.gethostname()
+    }
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
+@app.get("/info")
+def info():
+    return {
+        "hostname": socket.gethostname(),
+        "environment": os.getenv("ENVIRONMENT", "production"),
+    }
